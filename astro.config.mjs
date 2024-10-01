377a4ee67713c8eab2f19feb339463c4413d2c89
Levent <narrowbody@gmail.com>
2024-10-01 15:30:12 +0200

    added wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,13 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+	integrations: [tailwind(), icon()],
+	output: 'server',
+	adapter: cloudflare({
+		imageService: 'cloudflare',
+	}),
 });
