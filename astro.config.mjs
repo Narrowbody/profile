87e85409d7d3bf60ca1fbfbe617c1f4637f78d77
Levent <narrowbody@gmail.com>
2024-10-02 10:47:14 +0200

    Added node and force installed cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
-
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), icon()],
 	output: 'server',
+	imageService: 'cloudflare',
 	adapter: cloudflare({
 		imageService: 'cloudflare',
 	}),
